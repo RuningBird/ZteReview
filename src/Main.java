f1a249acc6916dd5f5c4695caae4ed1a6abcdc13
admin <ipowerink@outlook.com>
2017-09-06 21:13:34 +0800

    冒泡，快速（交换）；希尔（插入）

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,50 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         MySort ms = new MySort();
-        ms.shellSort();
+        ms.qkSort(0, ms.arr.length - 1);
         ms.showArr("排序后：");
     }
-
-
 }
 
 class MySort {
-    //    int[] arr = {5, 4, 3, 2, 1};
-    int[] arr = {26, 53, 67, 48, 57, 13, 48, 32, 60, 50};
+    int[] arr = {5, 4, 3, 2, 1};
+//    int[] arr = {26, 53, 67, 48, 57, 13, 48, 32, 60, 50};
 
+
+    /*交换类：快速排序*/
+    public void qkSort(int low, int high) {
+        if (low < high) {
+            int pos = qkPass(low, high);
+            qkSort(low, pos - 1);
+            qkSort(pos + 1, high);
+        }
+    }
+
+    private int qkPass(int low, int high) {
+        int x = arr[low];
+        while (low < high) {
+            while (low < high && x < arr[high]) {
+                high--;
+            }
+
+            if (low < high) {
+                arr[low] = arr[high];
+                low++;
+            }
+
+            while (low < high && x > arr[low]) {
+                low++;
+            }
+
+            if (low < high) {
+                arr[high] = arr[low];
+                high--;
+            }
+
+        }
+        arr[low] = x;
+        return low;
+    }
 
     /* 插入类：希尔排序
      * 是直接插入排序的改进
@@ -30,7 +63,6 @@ class MySort {
 
     /*希尔排序：直接插入排序*/
     private void shellInsert(int d) {
-
         for (int i = d; i < arr.length; i++) { //第二个元素开始
             int temp = arr[i];
             int j = i - d; //前一个元素的下表
